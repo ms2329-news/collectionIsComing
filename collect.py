ac8e471943c0d3f253e8ea129f7d38d9a6c4652b
KMicha <michael.kahle@yahoo.de>
2026-01-15 21:10:45 +0100

    Update collect.py

--- collect.py
+++ collect.py
@@ -82,6 +82,7 @@ def loadExistingNews(currentMonth):
     if(not fileDate in collectedNews):
         if(os.path.isfile(DATA_PATH / 'csv' / fileDate)):
             df = pd.read_csv(DATA_PATH / 'csv' / fileDate, delimiter=',',index_col='index')
+            df = df.drop(columns="url")
             collectedNews[fileDate] = df.to_dict('index')
         else:
             collectedNews[fileDate] = {}
